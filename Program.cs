8c841fae88268d82e4e0a89bc5c85e99e6b4e497
dedani4 <dedani4@gmail.com>
2023-02-19 15:39:03 +0300

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,15 +4,14 @@ Clear();
 Write("Enter few words splited with space: ");
 string[] stringArray = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-
 int count = 0;
 for (int i = 0; i < stringArray.Length; i++)
 {
-    if (stringArray[i].Length < 4) { count++; }
+    if (stringArray[i].Length < 4)
+        count++;
 }
 
 string[] resultArray = new string[count];
-
 
 for (int i = 0, j = 0; i < stringArray.Length; i++)
 {
@@ -23,12 +22,14 @@ for (int i = 0, j = 0; i < stringArray.Length; i++)
     }
 }
 
-Write("[" + resultArray[0] + ",");
-for (int i = 1; i < resultArray.Length; i++)
+Write("[");
+for (int i = 0; i < resultArray.Length; i++)
 {
-    Write(" " + resultArray[i] + ",");
+    if (i != resultArray.Length - 1)
+        Write(resultArray[i] + ", ");
+    else
+        Write(resultArray[i] + "]");
 }
-Write("]");
 
 
 
